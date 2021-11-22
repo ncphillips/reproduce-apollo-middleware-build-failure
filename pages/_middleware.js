468666e099217c10cec010fc18b70aa16c348013
ncphi <nolan@ncphi.com>
2021-11-22 12:38:06 -0400

    add apollo to build

---
+++ pages/_middleware.js
@@ -0,0 +1,8 @@
+import ApolloClient from 'apollo-boost';
+import { NextResponse } from 'next/server'
+
+export function middleware() {
+  const client = new ApolloClient({ uri: 'https://nx9zvp49q7.lp.gql.zone/graphql' });
+ 
+  return NextResponse.next()
+}
