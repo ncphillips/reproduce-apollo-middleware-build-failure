faf3bcaf150582bcd41eee3ddd1ca4030c4a29dd
ncphi <nolan@ncphi.com>
2021-11-22 12:40:01 -0400

    temp commit

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -3,6 +3,8 @@ import { NextResponse } from 'next/server'
 
 export function middleware() {
   const client = new ApolloClient({ uri: 'https://nx9zvp49q7.lp.gql.zone/graphql' });
+
+  console.log({ client })
  
   return NextResponse.next()
 }
